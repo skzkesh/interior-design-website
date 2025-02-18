642fbcbe756e68acc6e1e106dd0b36eeae0dd724
Keshia Poedjiono <keshiapud@gmail.com>
2025-02-17 23:08:26 -0400

    embedded whatsapp chat widget, closes #9

---
+++ JavaScript/main.js
@@ -0,0 +1,30 @@
+(function(w,d,s,o,f,js,fjs){
+    w[o]=w[o]||function(){(w[o].q=w[o].q||[]).push(arguments)};
+    js=d.createElement(s),fjs=d.getElementsByTagName(s)[0];
+    js.id=o; js.src=f; js.defer=1;
+    fjs.parentNode.insertBefore(js,fjs)
+})(window,document,"script","ww","https://toolassets.haptikapi.com/widget.js");
+
+ww("init",{
+    "debug": true,
+    "chatButton": {
+        "left": 20,
+        "bottom": 20,
+        "right": 20,
+        "position": "right",
+        "backgroundColor": "#00A68B",
+        "borderRadius": 40,
+        "withText": false,
+        "text": "",
+        "temp": 1
+    },
+    "chatWidget": {
+        "name": "INTI HPL",
+        "subHeader": "Typically replies within an hour",
+        "logo": "",
+        "greetingText": "\"Ada pertanyaan? Kami siap membantu! Klik WhatsApp untuk info lebih lanjut.\"",
+        "phone": "+6285107082202",
+        "backgroundColor": "#677871",
+        "async": 1
+    }
+});
